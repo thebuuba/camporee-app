f8741485fb9b85b97a7494dc39dd6a6183026417
thebuuba <natanaelpena012@gmail.com>
2026-09-07 17:51:22 -0400

    Use exact Camporee logo in splash

--- app/components/app-splash.tsx
+++ app/components/app-splash.tsx
@@ -18,14 +18,7 @@ export default function AppSplash(){
     <div className="app-splash-cloud cloud-left" />
     <div className="app-splash-cloud cloud-right" />
     <div className="app-splash-center">
-      <div className="app-splash-logo-mark" aria-hidden="true">
-        <span className="splash-sun"/>
-        <span className="splash-mountain splash-mountain-back"/>
-        <span className="splash-mountain splash-mountain-front"/>
-        <span className="splash-ground"/>
-        <span className="splash-tree">♠</span>
-        <span className="splash-tent"/>
-      </div>
+      <img className="app-splash-logo" src="/camporee-home-icon-v2.png" alt="Camporee" />
       <div className="app-splash-title">CAMPOREE</div>
       <div className="app-splash-tagline">MÁS QUE UN CAMPAMENTO,<br/>UNA HISTORIA JUNTOS</div>
       <div className="app-splash-progress" aria-hidden="true"><span /></div>
