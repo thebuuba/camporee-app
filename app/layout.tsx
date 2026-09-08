74fd642ab2dfd909d2c2060c8265d976142cf015
thebuuba <natanaelpena012@gmail.com>
2026-09-08 15:22:58 -0400

    Disable pinch zoom in PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,8 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
   viewportFit: "cover",
   themeColor: "#F4E7C9",
 };
