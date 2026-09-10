da79d5a85ca1a51efb7f99d8bfb68521c65a0377
thebuuba <natanaelpena012@gmail.com>
2026-09-10 11:11:06 -0400

    Load redesign design system globally

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import "./notifications.css";
 import "./users.css";
 import "./tasks.css";
 import "./desktop.css";
+import "./design-system.css";
 import PwaRegister from "./components/pwa-register";
 import ConnectionStatus from "./components/connection-status";
 import DataFreshness from "./components/data-freshness";
