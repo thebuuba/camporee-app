7219f3d554858a7e25c37bea58c19e4ea93bba6d
thebuuba <natanaelpena012@gmail.com>
2026-09-11 22:10:06 -0400

    Refresh PWA cache generation

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
-const CACHE='camporee-shell-v10';
-const STATIC=['/offline','/manifest.webmanifest?v=10'];
+const CACHE='camporee-shell-v11';
+const STATIC=['/offline','/manifest.webmanifest?v=11'];
 const PRIVATE_NAV_PREFIXES=['/','/program','/tasks','/more','/search'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(STATIC)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
